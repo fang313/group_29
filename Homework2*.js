30ee17196b878dd6fe7c25174cf708271b543e41
fang313 <103492272+fang313@users.noreply.github.com>
2022-04-19 10:28:38 +0300

    Update Homework2*.js

--- Homework2*.js
+++ Homework2*.js
@@ -3,14 +3,12 @@ let age_2 = 18
 let age_3 = 60 
 
 const checkAge = function(age) {
+   
 
-    if (age ==Number)
+    if (typeof(age)!=Number)
     console.log ("Error") 
     
-       else{ 
-
-        Number(checkAge)
-        Number(age)
+       else{  */
 
         if  (age < age_2) {
             console.log("You dont have access cause your age is " + age + "Its less then " + age_2) 
@@ -26,9 +24,34 @@ const checkAge = function(age) {
             console.log("Technical work") 
         } 
     }
-}
+} 
 
 checkAge (17)
+console.log (typeof age)
 checkAge (18)
+console.log (typeof age)
 checkAge (61)
+console.log (typeof age)
 checkAge ('2')
+console.log (typeof age)
+
+/* let EnterAge = prompt ('Enter you age')
+checkAge (EnterAge)
+ */
+
+/* 
+1*:
+Преобразовать написанный код в 26-33 пунктах в функцию, принимающую на вход возраст.
+Пример: const checkAge = function(age) {
+Ваши преобразования
+}
+Вывести в консоль результат работы функции с возрастами 17, 18, 61
+
+2*:
+Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных. И если он не Number - кидалась ошибка.
+
+3**:
+Преобразовать 2* таким образом, чтобы значение '2' (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number
+
+4***:
+Преобразовать задание 3* таким образом, чтобы возраст вводится используя функцию prompt в привязанной верстке */
