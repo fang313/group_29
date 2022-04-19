f1d70e2343d3b85171d7640c4e9f0f66d607c97d
fang313 <103492272+fang313@users.noreply.github.com>
2022-04-19 11:47:55 +0300

    Update Homework2*.js

--- Homework2*.js
+++ Homework2*.js
@@ -1,56 +1,56 @@
-    let age_1 = 10 
-    let age_2 = 18 
-    let age_3 = 60 
+let age_1 = 10 
+let age_2 = 18 
+let age_3 = 60 
 
-    let checkAge = function(age) {
+let checkAge = function(age) {
+   
+    if (typeof age !== 'number') 
     
-        if (typeof age !== 'number') 
-        
-            Number(age)
-        /* {console.error('Not a number')} */
+        /* Number(age) */
+    {console.error('Not a number')} 
 
 
-            else{ 
+        else{ 
 
-            if  (age < age_2) {
-                console.log("You dont have access cause your age is " + age + "Its less then " + age_2) 
-            }   
-            else if (age >= age_2 && age < age_3) {
-                console.log("Welcome !")
-            }
-
-            else if (age > age_3) {
-                console.log("Keep calm and look Culture channel")
-            }    
-            else{
-                console.log("Technical work") 
-            } 
+        if  (age < age_2) {
+            console.log("You dont have access cause your age is " + age + "Its less then " + age_2) 
+        }   
+        else if (age >= age_2 && age < age_3) {
+            console.log("Welcome !")
         }
-    } 
 
-    /* checkAge (17)
-    checkAge (18)
-    checkAge (61)
-    checkAge ('2') */
-
-
-    let enterage = prompt ('Enter you age', '');
-    checkAge (enterage)
-
-
-    /* 
-    1*:
-    Преобразовать написанный код в 26-33 пунктах в функцию, принимающую на вход возраст.
-    Пример: const checkAge = function(age) {
-    Ваши преобразования
+        else if (age > age_3) {
+            console.log("Keep calm and look Culture channel")
+        }    
+        else{
+            console.log("Technical work") 
+        } 
     }
-    Вывести в консоль результат работы функции с возрастами 17, 18, 61
+} 
 
-    2*:
-    Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных. И если он не Number - кидалась ошибка.
+checkAge (17)
+checkAge (18)
+checkAge (61)
+checkAge ('2')
 
-    3**:
-    Преобразовать 2* таким образом, чтобы значение '2' (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number
 
-    4***:
-    Преобразовать задание 3* таким образом, чтобы возраст вводится используя функцию prompt в привязанной верстке */
+/* let EnterAge = prompt ('Enter you age','');
+checkAge (EnterAge) */
+
+
+/* 
+1*:
+Преобразовать написанный код в 26-33 пунктах в функцию, принимающую на вход возраст.
+Пример: const checkAge = function(age) {
+Ваши преобразования
+}
+Вывести в консоль результат работы функции с возрастами 17, 18, 61
+
+2*:
+Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных. И если он не Number - кидалась ошибка.
+
+3**:
+Преобразовать 2* таким образом, чтобы значение '2' (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number
+
+4***:
+Преобразовать задание 3* таким образом, чтобы возраст вводится используя функцию prompt в привязанной верстке */
