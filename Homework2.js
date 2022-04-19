e15e1386cd98f3eb9204c5e4f75ffcdde4fd683a
fang313 <103492272+fang313@users.noreply.github.com>
2022-04-19 11:20:46 +0300

    Update Homework2.js

--- Homework2.js
+++ Homework2.js
@@ -29,7 +29,7 @@ else if (age_1 >= age_2 && age_1 < age_3) {
     console.log("Welcome !")
 }
 
-else if (age_1 > age_3) {
+else if (age_1 >= age_3) {
     console.log("Keep calm and look Culture channel")
 }    
 else{
