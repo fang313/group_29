d09da26da4f5692b0bbefed601708940b4174667
fang313 <103492272+fang313@users.noreply.github.com>
2022-04-19 13:03:07 +0300

    Update Homework2*.js

--- Homework2*.js
+++ Homework2*.js
@@ -3,54 +3,30 @@ let age_2 = 18
 let age_3 = 60 
 
 let checkAge = function(age) {
-   
-    if (typeof age !== 'number') 
     
-        /* Number(age) */
-    {console.error('Not a number')} 
+    agenumber = Number (age);
+    if (typeof agenumber == 'number' && (age < age_2)) 
+        console.log("You dont have access cause your age is " + age + "Its less then " + age_2)
 
-
-        else{ 
-
-        if  (age < age_2) {
-            console.log("You dont have access cause your age is " + age + "Its less then " + age_2) 
-        }   
-        else if (age >= age_2 && age < age_3) {
-            console.log("Welcome !")
-        }
-
-        else if (age > age_3) {
-            console.log("Keep calm and look Culture channel")
-        }    
-        else{
-            console.log("Technical work") 
-        } 
+    else if (agenumber >= age_2 && age < age_3) {
+        console.log("Welcome !")
     }
-} 
+    else if (agenumber > age_3) {
+            console.log("Keep calm and look Culture channel")
+    }
+    
+    else if (typeof agenumber !== 'number') {
+        console.log("Error") 
+    }  
+    else{
+    console.log("Technical work") 
+    } 
+}
 
 checkAge (17)
 checkAge (18)
 checkAge (61)
 checkAge ('2')
 
-
-/* let EnterAge = prompt ('Enter you age','');
-checkAge (EnterAge) */
-
-
-/* 
-1*:
-Преобразовать написанный код в 26-33 пунктах в функцию, принимающую на вход возраст.
-Пример: const checkAge = function(age) {
-Ваши преобразования
-}
-Вывести в консоль результат работы функции с возрастами 17, 18, 61
-
-2*:
-Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных. И если он не Number - кидалась ошибка.
-
-3**:
-Преобразовать 2* таким образом, чтобы значение '2' (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number
-
-4***:
-Преобразовать задание 3* таким образом, чтобы возраст вводится используя функцию prompt в привязанной верстке */
+let EnterAge = prompt ('Enter you age','');
+checkAge (EnterAge)
