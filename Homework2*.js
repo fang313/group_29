084c653f7e8505e00ae0ba582cdf7d4b31ffd807
fang313 <103492272+fang313@users.noreply.github.com>
2022-04-19 08:41:43 +0300

    Update Homework2*.js

--- Homework2*.js
+++ Homework2*.js
@@ -2,33 +2,33 @@ let age_1 = 10
 let age_2 = 18 
 let age_3 = 60 
 
- if (age_1 < age_2) {
-    console.log("You dont have access cause your age is " + age_1 + "Its less then " + age_2) 
-}   
-else if (age_1 >= age_2 && age_1 < age_3) {
-    console.log("Welcome !")
+const checkAge = function(age) {
+
+    if (age ==Number)
+    console.log ("Error") 
+    
+       else{ 
+
+        Number(checkAge)
+        Number(age)
+
+        if  (age < age_2) {
+            console.log("You dont have access cause your age is " + age + "Its less then " + age_2) 
+        }   
+        else if (age >= age_2 && age < age_3) {
+            console.log("Welcome !")
+        }
+
+        else if (age > age_3) {
+            console.log("Keep calm and look Culture channel")
+        }    
+        else{
+            console.log("Technical work") 
+        } 
+    }
 }
 
-else if (age_1 > age_3) {
-    console.log("Keep calm and look Culture channel")
-}    
-else{
-    console.log("Technical work") 
-} 
-
-/* 
-1*:
-Преобразовать написанный код в 26-33 пунктах в функцию, принимающую на вход возраст.
-Пример: const checkAge = function(age) {
-Ваши преобразования
-}
-Вывести в консоль результат работы функции с возрастами 17, 18, 61
-
-2*:
-Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных. И если он не Number - кидалась ошибка.
-
-3**:
-Преобразовать 2* таким образом, чтобы значение '2' (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number
-
-4***:
-Преобразовать задание 3* таким образом, чтобы возраст вводится используя функцию prompt в привязанной верстке */
+checkAge (17)
+checkAge (18)
+checkAge (61)
+checkAge ('2')
